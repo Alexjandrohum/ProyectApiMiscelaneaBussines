b5b03aa856bef4078d7c7ee11a26b71bf919f48b
Alexjandrohum <alexjandrohum@Alexjandrohum.local>
2020-11-01 15:49:22 -0600

    Initial Proyect Api Rest

---
+++ src/main/java/ProyectApiMiscelaneaBussines/ProyectApiMiscelaneaBussines/ProyectApiMiscelaneaBussinesApplication.java
@@ -0,0 +1,13 @@
+package ProyectApiMiscelaneaBussines.ProyectApiMiscelaneaBussines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectApiMiscelaneaBussinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectApiMiscelaneaBussinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectApiMiscelaneaBussines/ProyectApiMiscelaneaBussines/ProyectApiMiscelaneaBussinesApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectApiMiscelaneaBussines.ProyectApiMiscelaneaBussines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectApiMiscelaneaBussinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
